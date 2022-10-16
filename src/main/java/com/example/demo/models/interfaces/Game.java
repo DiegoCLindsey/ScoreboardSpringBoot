71512667361ba68533857d2a9703040c55a3f871
Diego <DiegoClindsey@gmail.com>
2022-10-16 16:09:37 +0200

    Create Game interface

---
+++ src/main/java/com/example/demo/models/interfaces/Game.java
@@ -0,0 +1,29 @@
+package com.example.demo.models.interfaces;
+
+/**
+ * A game is defined by the teams which plays it and its scores.
+ */
+public interface Game {
+    /**
+     *
+     * @return the {@link Team} object which defines the 'Away Team'
+     */
+    Team getAwayTeam();
+    /**
+     * 
+     * @return the {@link Team} object which holds the 'Home team'
+     */
+    Team getHomeTeam();
+
+    /**
+     * 
+     * @return the number of points scored during the game
+     */
+    int  getScore();
+
+    /**
+     * Sets the score of the game given a {@link Score} pair
+     * @param newScore the new Score of the game
+     */
+    void setScore(Score newScore);
+}
