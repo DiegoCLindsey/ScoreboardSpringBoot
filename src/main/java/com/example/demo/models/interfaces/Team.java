67b6cbe03c398ee28a7e6e037ef452333eb64e26
Diego <DiegoClindsey@gmail.com>
2022-10-16 14:54:44 +0200

    comments on team interface

--- src/main/java/com/example/demo/models/interfaces/Team.java
+++ src/main/java/com/example/demo/models/interfaces/Team.java
@@ -6,5 +6,8 @@ package com.example.demo.models.interfaces;
  * A team object should have a name which we should be able to get
  */
 public interface Team {
+    /**
+     * The Team name should be only readable
+     */
     public String getName();
 }
