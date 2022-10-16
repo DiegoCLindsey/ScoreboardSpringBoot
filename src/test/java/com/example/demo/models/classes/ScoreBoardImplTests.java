597579a02dc206b77d848f88d5897c9adead2383
Diego <DiegoClindsey@gmail.com>
2022-10-16 17:11:36 +0200

    Fix imports

--- src/test/java/com/example/demo/models/classes/ScoreBoardImplTests.java
+++ src/test/java/com/example/demo/models/classes/ScoreBoardImplTests.java
@@ -1,10 +1,9 @@
 package com.example.demo.models.classes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
